a1ceca976eec6d50565835ed3b3d4929e09e57fc
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-06-01 01:26:00 -0500

    Pushed some assets. Partially set up file structure.
    Assets & tailwind config not pushed in public preview ver.

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <main className="min-h-screen border-red-500 border-2 border-solid !m-0 !p-0">
+
+      </main>
+    </>
+  );
+}
