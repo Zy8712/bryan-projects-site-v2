4e8168615d3b6e0d729aab849e02ce055b876a28
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-06-04 23:45:00 -0400

    Pushing changes to project card.
    Specifics:
    - created link for on-site project details page which'll be implemented later
    - adjusted image rendering/loading for challenge link button
    - altered challenge link button to take up less room and have a cleaner hover indicator
    - adjusted padding so project details (date, title) have more room
    
    Some assets, code & tailwind config may not be pushed in public preview ver.

--- src/components/project-card-components/ProjectDetails.jsx
+++ src/components/project-card-components/ProjectDetails.jsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 
 export default function ProjectDetails(props) {
     return (
         <>
-            <div className="w-full flex flex-col justify-between items-center px-6 custom-sm:px-11 text-center relative">
-                <span className="block mt-4 mb-2 text-2xl font-medium">{props.name}</span>
+            <div className="w-full flex flex-col justify-between items-center px-6 custom-sm:px-9 text-center relative">
+                <span className="w-full block mt-4 mb-2 text-2xl font-medium">{props.name}</span>
                 <p className="mb-4">
                     {props.project_status}:&nbsp;
                     {props.date}&nbsp;
@@ -16,16 +17,48 @@ export default function ProjectDetails(props) {
                     {props.children}
                 </div>
 
-                <div className={`w-[30px] ${props.source != 0 ? 'flex' : 'hidden'} flex-col absolute top-4 right-[2px] custom-sm:right-2 
-                bg-white border-2 border-solid border-transparent rounded-full hover:bg-nav-gradient-1`}>
-                    <a href={props.links.challenge_link} target="_blank">
-                        <img src="/icons/general-icons/info-svgrepo-com.svg" alt="info_icon" className="mb-1" />
-                        <img src="/icons/general-icons/frontend-mentor-logo-transparent.png" alt="frontendmentor_icon" className={`${props.source == 1 ? 'inline' : 'hidden'}`} />
-                        <img src="/icons/general-icons/icodethis-logo.jpg" alt="icodethis_icon" className={`${props.source == 2 ? 'inline' : 'hidden'} rounded-full`} />
-                        <img src="/icons/general-icons/codewellcc-logo.jpg" alt="codewellcc_icon" className={`${props.source == 3 ? 'inline' : 'hidden'} border-[0.3px] border-gray-400 border-solid rounded-full`} />
-                        <img src="/icons/general-icons/frontendpro-logo.png" alt="frontendpro_icon" className={`${props.source == 4 ? 'inline' : 'hidden'} `}/>
-                        <img src="/icons/general-icons/youtube-svgrepo-com.svg" alt="frontendpro_icon" className={`${props.source == 5 ? 'inline' : 'hidden'} `}/>
-                    </a>
+
+                <div className={`w-9 h-9 ${props.source != 0 ? 'flex' : 'hidden'} flex-col justify-center items-center absolute top-3 right-[2px] custom-sm:right-2 
+                bg-white hover:bg-gradient-to-tr from-gradient-blue to-gradient-purple border-white border-[0.5px] border-dashed rounded-full`}>
+                    <div className="w-7 h-7 flex flex-col justify-center items-center bg-white rounded-full">
+                        <a href={props.links.challenge_link} target="_blank">
+                            <Image
+                                src="/icons/project-source-icons/frontend-mentor-logo-transparent.png"
+                                alt="frontendmentor_icon"
+                                className={`${props.source == 1 ? 'inline' : 'hidden'} rounded-full`}
+                                width={28}
+                                height={28}
+                            />
+                            <Image
+                                src="/icons/project-source-icons/icodethis-logo.jpg"
+                                alt="icodethis_icon"
+                                className={`${props.source == 2 ? 'inline' : 'hidden'} rounded-full`}
+                                width={28}
+                                height={28}
+                            />
+                            <Image
+                                src="/icons/project-source-icons/codewellcc-logo.jpg"
+                                alt="codewellcc_icon"
+                                className={`${props.source == 3 ? 'inline' : 'hidden'} rounded-full`}
+                                width={28}
+                                height={28}
+                            />
+                            <Image
+                                src="/icons/project-source-icons/frontendpro-logo.png"
+                                alt="frontendpro_icon"
+                                className={`${props.source == 4 ? 'inline' : 'hidden'} rounded-full `}
+                                width={28}
+                                height={28}
+                            />
+                            <Image
+                                src="/icons/project-source-icons/youtube-svgrepo-com.svg"
+                                alt="frontendpro_icon"
+                                className={`${props.source == 5 ? 'inline' : 'hidden'} rounded-full`}
+                                width={28}
+                                height={28}
+                            />
+                        </a>
+                    </div>
                 </div>
             </div >
 
