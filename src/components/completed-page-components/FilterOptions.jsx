1116f4fb95265143f28ca0de9c6c0e20c8ea8525
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-06-06 23:03:00 -0400

    pushed primary filters
    Specifics:
    - pushed filter categories
    - pushed filter options (language, source, etc)
    - pushed search bar
    
    Some assets, code, files & tailwind config may not be pushed in public preview ver.

---
+++ src/components/completed-page-components/FilterOptions.jsx
@@ -0,0 +1,31 @@
+
+export default function FilterOptions(){
+
+    const filterOptions = [
+        { iconClass: "las la-file-code", filterText: "HTML, CSS, SASS Excl." },
+        { iconClass: "lab la-js-square", filterText: "Vanilla JS" },
+        { iconClass: "lab la-react", filterText: "React" },
+        { iconClass: "las la-wind", filterText: "Tailwind CSS" },
+        { iconClass: "las la-file-alt", filterText: "JSON" },
+        { iconClass: "las la-cog", filterText: "API" },
+        { iconClass: "lab la-vuejs", filterText: "Vue" },
+        { iconClass: "las la-rocket", filterText: "Astro" },
+        { iconClass: "lab la-angular", filterText: "Angular" },
+
+        { iconClass: "las la-laptop-code", filterText: "Frontend Mentor" },
+        { iconClass: "las la-check-circle", filterText: "Codewell" },
+        { iconClass: "las la-user-tie", filterText: "Frontend Pro" },
+        { iconClass: "las la-file-code", filterText: "iCodeThis" },
+        { iconClass: "lab la-youtube", filterText: "YouTube" },
+        { iconClass: "las la-user", filterText: "Personal" },
+
+        { iconClass: "las la-star", filterText: "Featured" },
+        { iconClass: "las la-meteor", filterText: "Recent" },
+    ];
+
+    return(
+        <>
+        
+        </>
+    );
+}
