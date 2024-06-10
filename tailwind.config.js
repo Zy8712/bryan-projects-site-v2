18e42a831fa421b649382333d1556847436a6973
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-06-10 12:28:00 -0400

    Implemented bug fixes for filter chips and previews.
    Specifics:
    - bug would occur when a category button like Tech, Source, or Other would be clicked for a second time after a options filter was already selected
    - it would result in the filtered projects going back to display all projects
    - this was caused by the calling of the filterProjects() function whenever Tech, Source, or Other was clicked, it would result in the data being returned from the function being allProjects
    - fixed by adding pre-conditions for the calling of the filterProjects() function from the setCategoryFilter reducer
    - fixed swapped previews for the nike and iphone projects caused by the swapped naming of their respective preview images
    
    Prior Commit:
    - bug caused by the addition of redux features in the card rendering
    - Upcoming and In Progress pages did not have access to the redux store and they used the same card rendering file
    - temp. fix by giving them <Provider> tags allowing them access to the redux store
    
    Some assets, code, files & tailwind config may not be pushed in public preview ver.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,59 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        'light-gray': 'hsl(217, 12%, 63%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+
+        'translucent-white': 'rgba(255,255,255,0.55)',
+        'translucent-white-2': 'rgba(255,255,255,0.15)',
+
+        'gradient-blue': 'hsl(192, 100%, 67%)',
+        'gradient-purple': 'hsl(280, 87%, 65%)',
+        'nav-gradient-1': '#DDBDFC',
+        'nav-gradient-2': '#96C8FB',
+        'filter-gradient-1': '#36D1DC',
+        'filter-gradient-2': '#5B86E5',
+        'light-gray': 'hsl(217, 12%, 63%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)',
+
+        'fem-newbie': 'rgb(106, 190, 205)',
+        'fem-junior': 'rgb(170, 215, 66)',
+        'fem-intermediate': 'rgb(241, 182, 4)',
+        'fem-advanced': 'rgb(244, 137, 37)',
+        'fem-guru': 'rgb(237, 44, 73)',
+
+        'fem-featured-1': 'hsl(273, 75%, 66%)',
+        'fem-featured-2': 'hsl(240, 73%, 65%)',
+
+        'translucent-black-1': 'rgba(0, 0, 0, 0.45)',
+        'translucent-white-3': 'rgba(255, 255, 255, 0.45)',
+        'translucent-black-2': 'rgba(0, 0, 0, 0.4)',
+      },
+      fontFamily: {
+        'theme-orbitron': ['Orbitron', 'sans-serif'],
+        'theme-oxanium': ['Oxanium', 'sans-serif'],
+        'theme-rubik': ['Rubik', 'sans-serif'], 
+        'theme-barlow': ['Barlow', 'sans-serif'],
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'custom-background' : "url('/backgrounds/mesh-943-1920x1080.png')",
+        'custom-background-2' : "url('/backgrounds/mesh-943-480x853.png')",
+      },
+      screens: {
+        'custom-xl' : '1200px',
+        'custom-lg' : '900px',
+        'custom-md' : '850px',
+        'custom-sm-ex' : '550px',
+        'custom-sm' : '440px',
+      },
+      scale: {
+        '108': '1.08',
       },
     },
   },
