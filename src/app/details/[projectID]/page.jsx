cca19e518c27bdfb15eb68b19753918077be296d
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-06-21 21:28:00 -0400

    Pushed work on details page.
    Specifics:
    - laid out parts of page
    - began addition of completion time into data json
    - designed and implemented grid layout for page
    - implemented new icons and quick project details boxes (4 grid boxes on right of page)
    - quick details covers project source, completion date, completion time, project difficulty
    
    Note: Some assets, code, files & tailwind config may not be pushed in public preview ver.

--- src/app/details/[projectID]/page.jsx
+++ src/app/details/[projectID]/page.jsx
@@ -1,5 +1,9 @@
 import DetailsPageLayout from "@/layouts/DetailsPageLayout";
 import all_projects_data from "@/data/all-projects.json";
+import DetailsPage_SourceBox from "@/components/details-page-components/DetailsPage_SourceBox";
+import DetailsPage_DateBox from "@/components/details-page-components/DetailsPage_DateBox";
+import DetailsPage_DifficultyBox from "@/components/details-page-components/DetailsPage_DifficultyBox";
+import DetailsPage_CompletionTimeBox from "@/components/details-page-components/DetailsPage_CompletionTimeBox";
 
 export default function Details({ params }) {
 
@@ -7,13 +11,47 @@ export default function Details({ params }) {
 
     return (
         <DetailsPageLayout>
-            <div className="relative w-full h-96 overflow-hidden rounded-b-[48px]">
-                <img src={project.preview_image} alt="" className="absolute opacity-25" />
+            <div className="relative w-full h-80 flex flex-col justify-center items-center overflow-hidden rounded-b-[48px] px-5">
+                <img src={project.preview_image} alt="" className="absolute -z-20 w-full opacity-35" />
+                <h1 className="text-white text-7xl font-theme-orbitron font-semibold leading-[80px] text-center">
+                    {project.name}
+                </h1>
             </div>
-            <div className="w-full h-full min-h-screen border-white border-2 border-solid">
-                <p className="text-white font-semibold text-2xl">Info on {project.name}</p>
-                <p className="text-white">{project.description}</p>
+
+            <div className="w-full h-8 flex justify-end items-center">
+                <span className="text-white font-semibold font-theme-oxanium text-xs">Custom ID: {project.id}</span>
             </div>
+
+            <div className="w-full h-full flex justify-between">
+
+                <div className="w-[calc(100%-340px)] h-80 border-white border-2 border-solid p-2">
+                    <div className="w-16 h-16 border-white border-2 border-solid rounded-full">
+
+                    </div>
+                </div>
+
+                <div className="w-80 h-80 flex justify-around items-center flex-wrap font-theme-oxanium bg-white bg-opacity-20 rounded-3xl">
+                    <DetailsPage_SourceBox source={project.source} />
+
+                    <DetailsPage_DateBox date={project.date} />
+
+                    <DetailsPage_DifficultyBox source={project.source} difficulty={project.difficulty} />
+
+                    <DetailsPage_CompletionTimeBox time={project.completion_time_hours} />
+                </div>
+
+            </div>
+
+            <div className="mt-8 w-full h-96 border-white border-2 border-solid">
+
+            </div>
+
+            <div className="mt-8 w-full h-full min-h-screen border-white border-2 border-solid">
+
+            </div>
+
+
+
         </DetailsPageLayout>
     );
 }
