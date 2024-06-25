e147ef4e7aba8861983a7d3b704ac3ad3f49718d
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-06-25 02:59:00 -0400

    Removed duplicate copies. Added missing icon. Testing new hero.
    Specifics:
    - added missin odion project icon
    - pushed new hero for home page, not fully mobile responsive yet
    - removed duplicate files for details page
    
    Note: Some assets, code, files & tailwind config may not be pushed in public preview ver.

--- src/components/home-page-components/HomePage_Hero.jsx
+++ src/components/home-page-components/HomePage_Hero.jsx
@@ -1,45 +1,63 @@
 'use client'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-
 import SocialIcons from "@/components/shared-components/navbar-components/SocialIconsReduced";
 
 export default function HomeHero() {
+
+    const textShadow = "[text-shadow:_1.5px_0px_white,_0px_1.5px_white,_1.5px_1.5px_white,_-1.5px_-1.5px_white,_-1.5px_0px_white,_0px_-1.5px_white,_-1.5px_1.5px_white,_1.5px_-1.5px_white]";
+
     return (
         <>
-            <div className="relative w-full h-full min-h-screen flex justify-center items-center border-red-500 border-2 border-solid">
+            <div className="relative w-full h-full min-h-[850px] flex justify-center items-center border-red-500 border-2 border-solid">
 
-                <div className="w-full max-w-[1400px] h-full min-h-screen flex flex-col justify-center items-center font-theme-orbitron text-white text-center">
+                <div className="w-full max-w-[1400px] h-full min-h-[850px] flex flex-col custom-xl:flex-row-reverse justify-between items-center py-20 custom-xl:py-0">
 
-                    <div className="absolute top-16 bottom-auto sm:top-auto sm:bottom-7 right-[48%] translate-x-[50%]">
-                        <DotLottieReact
-                            src="/animations/Animation1719195618226.lottie"
-                            loop={false}
-                            autoplay
-                            speed={0.4}
-                            className="w-[650px] sm:w-[500px]"
-                        />
+                    <div className="w-full max-w-full custom-xl:max-w-[600px] h-full min-h-[375px] custom-xl:min-h-[850px] flex flex-col justify-end custom-xl:justify-center items-center border-white border-2 border-solid">
+                        <div className="relative w-full custom-xl:max-w-full h-[375px] custom-xl:h-[475px] border-green-500 border-2 border-solid overflow-hidden">
+                            <DotLottieReact
+                                src="/animations/Animation1719195618226.lottie"
+                                loop={false}
+                                autoplay
+                                speed={0.4}
+                                className="absolute top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%] w-[calc(100%+750px)] max-w-[1000px] custom-xl:max-w-[1400px] border-orange-500 border-2 border-solid"
+                            />
+                        </div>
                     </div>
 
-                    <h1 className="z-50 w-full mt-0 custom-sm:mt-64 sm:mt-14 text-[40px] custom-sm:text-5xl sm:text-6xl custom-md:text-[80px] lg:text-8xl font-bold custom-sm:font-semibold leading-[48px] custom-sm:leading-[56px] sm:leading-[72px] custom-md:leading-[100px] lg:leading-[120px]">
-                        Explore Bryan&apos;s{' '}
-                        <span className="text-blue-400 [text-shadow:_2px_0px_white,_0px_2px_white,_2px_2px_white,_-2px_-2px_white,_-2px_0px_white,_0px_-2px_white,_-2px_2px_white,_2px_-2px_white]">
-                            Completed
-                        </span>{' '}
-                        and{' '}
-                        <span className="text-blue-400 [text-shadow:_2px_0px_white,_0px_2px_white,_2px_2px_white,_-2px_-2px_white,_-2px_0px_white,_0px_-2px_white,_-2px_2px_white,_2px_-2px_white]">
-                            Upcoming Projects
-                        </span>
-                    </h1>
 
-                    <div className="z-50 w-auto sm:w-[480px] md:w-[760px] lg:w-[980px] h-36 sm:h-auto mt-10 sm:mt-20 flex flex-col sm:flex-row justify-between sm:justify-between items-center transition-all duration-500 ease-in-out">
-                        <a href="/featured" className="flex justify-center items-center w-72 sm:w-auto h-16 sm:h-auto px-0 sm:px-5 py-0 sm:py-3 text-xl md:text-xl lg:text-2xl font-semibold uppercase border-white border-4 border-solid rounded-lg hover:bg-gradient-to-tr from-blue-500 to-purple-500">
-                            <i className="las la-star text-3xl md:text-3xl lg:text-4xl mr-2"></i>
-                            Featured Items
-                        </a>
-                        <a href="/completed" className="flex justify-center items-center w-72 sm:w-auto h-16 sm:h-auto px-0 sm:px-5 py-0 sm:py-3 text-xl md:text-xl lg:text-2xl font-semibold uppercase border-white border-4 border-solid rounded-lg hover:bg-gradient-to-tr from-blue-500 to-purple-500">
-                            <i className="las la-compass text-3xl md:text-3xl lg:text-4xl mr-2"></i>
-                            Explore All
-                        </a>
+                    <div className="w-full custom-xl:w-[calc(100%-600px)] min-h-[400px] custom-xl:min-h-[850px] flex flex-col justify-center border-black border-2 border-solid">
+                        
+                        <div className="w-full max-w-full custom-xl:max-w-[800px] h-full min-h-[400px] custom-xl:min-h-[475px] flex flex-col justify-center items-center custom-xl:items-start text-center custom-xl:text-start  border-pink-500 border-2 border-solid">
+                            <h1 className="w-full max-w-[800px] custom-xl:max-w-full text-6xl custom-xl:text-7xl leading-[76px] font-semibold font-theme-orbitron text-white">
+                                Explore{' '}
+                                <span className={`text-blue-400 ${textShadow}`}>
+                                    Completed
+                                </span>{' '}
+                                and{' '}
+                                <span className={`text-blue-400 ${textShadow}`}>
+                                    Upcoming
+                                </span>{' '}
+                                Projects
+                            </h1>
+
+                            <p className="mt-8 w-full max-w-[750px] text-white font-theme-oxanium text-lg">
+                                Explore my completed, ongoing, and upcoming projects. From design to code to the final product, see how each idea comes to life. Check out featured items, follow the progress of current projects, and get a sneak peek at what's next. Dive in and discover the journey from concept to completion!
+                            </p>
+
+                            <div className="mt-8 w-[400px] h-12 flex justify-between font-theme-orbitron text-white">
+                                <a href="/featured" className="w-44 h-12 flex justify-center items-center font-medium text-lg p-1 border-white border-2 border-solid rounded-lg hover:hover:bg-gradient-to-tr from-blue-500 to-purple-500">
+                                    <i className="las la-star mr-2"></i>
+                                    Featured
+                                    <i className="las la-angle-double-right ml-2"></i>
+                                </a>
+
+                                <a href="/completed" className="w-44 h-12 flex justify-center items-center font-medium text-lg p-1 border-white border-2 border-solid rounded-lg hover:hover:bg-gradient-to-tr from-blue-500 to-purple-500">
+                                    <i className="las la-compass text-xl mr-2"></i>
+                                    Explore
+                                    <i className="las la-angle-double-right ml-2"></i>
+                                </a>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
