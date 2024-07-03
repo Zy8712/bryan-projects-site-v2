3367a8fb4e6b67726cd11f40b8fc67e9827865ea
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-07-03 05:19:34 +0000

    Added icons. Improved contact ui/ux.
    Specifics:
    - contact form now removes placeholder text upon user focusing on it (click on it)
    - contact form now shows a light translucent white background on the selected textbox
    - centering of text and icon of submit button for contact form is now fixed
    - added extra padding to bottom of textbox for contact form message
    - added missing icons for link boxes
    - added some extra info for link boxes
    - altered gradient direction for improved icon visibility

--- src/components/home-page-components/HomePage_SiteFeatures.jsx
+++ src/components/home-page-components/HomePage_SiteFeatures.jsx
@@ -1,9 +1,16 @@
 import HomePageSectionLayout from "@/layouts/HomePageSectionLayout";
+import HomePageSectionHeader from "./HomePageSectionHeader";
 
 export default function HomePage_SiteFeatures() {
     return (
         <>
-            <HomePageSectionLayout></HomePageSectionLayout>
+            <HomePageSectionLayout>
+                <HomePageSectionHeader>Site Features</HomePageSectionHeader>
+                <div className="w-full h-full min-h-96 flex justify-between border-white border-2 border-solid">
+
+                </div>
+
+            </HomePageSectionLayout>
         </>
     );
 }
