d23c11bb8636efab81cdbbd85e6a04a8742d6251
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-07-24 21:08:00 -0400

    Worked a bit more on tech stack section on home page.
    Note: Some assets, code, files & tailwind config may not be pushed in public preview ver.

--- src/components/home-page-components/HomePage_SiteFeatures.jsx
+++ src/components/home-page-components/HomePage_SiteFeatures.jsx
@@ -1,23 +1,57 @@
+'use client'
+import { useRef } from "react";
+
 import HomePageSectionLayout from "@/layouts/HomePageSectionLayout";
 import HomePageSectionHeader from "./HomePageSectionHeader";
 
 export default function HomePage_SiteFeatures() {
+
+    const items = [
+        "Search",
+        "Basic Filtering",
+        "Advanced Filtering",
+        "Project Details",
+        "Preview Gallery",
+        "Relative Links",
+        "Project Suggestions"
+    ];
+
+    const containerRef = useRef(null);
+
+    const scrollLeft = () => {
+        containerRef.current.scrollBy({ left: -410, behavior: 'smooth' });
+    };
+
+    const scrollRight = () => {
+        containerRef.current.scrollBy({ left: 410, behavior: 'smooth' });
+    };
+
     return (
         <>
-            <HomePageSectionLayout>
-                <HomePageSectionHeader>Site Features</HomePageSectionHeader>
-                <div className="w-full h-full min-h-96 flex justify-between flex-wrap border-white border-2 border-solid">
-                    <div className="w-28 h-28">Search</div>
-                    <div className="w-28 h-28">Basic Filtering</div>
+            <div className="w-full h-full min-h-96 flex flex-col items-center py-16">
+                <div className="relative w-full h-full flex flex-col min-h-96">
 
-                    <div className="w-28 h-28">Advanced Filtering</div>
-                    <div className="w-28 h-28">Project Details</div>
-                    <div className="w-28 h-28">Preview Gallery</div>
-                    <div className="w-28 h-28">Relative Links</div>
-                    <div className="w-28 h-28">Project Suggestions</div>
+                    <HomePageSectionHeader>Site Features</HomePageSectionHeader>
+                    <div className="relative w-full h-full min-h-96 flex gap-x-6 overflow-hidden" ref={containerRef}>
+                        {items.map((item, index) => (
+                            <div key={index} className="w-72 h-96 flex-shrink-0 flex-grow-0 border-white border-2 border-solid rounded-2xl flex justify-center items-center">
+                                {item}
+                            </div>
+                        ))}
+
+                    </div>
+
+                    <div className="absolute top-3 right-5 w-24 flex justify-between">
+                        <button className="w-10 h-10 border-white border-2 border-solid rounded-full" onClick={scrollLeft}>←
+
+                        </button>
+                        <button className="w-10 h-10 border-white border-2 border-solid rounded-full" onClick={scrollRight}>→
+
+                        </button>
+                    </div>
+
                 </div>
-
-            </HomePageSectionLayout>
+            </div>
         </>
     );
 }
--- src/utils/projectTechIcons.js
+++ src/utils/projectTechIcons.js
@@ -12,9 +12,29 @@ export const techIcons = [
     { iconImage: "/icons/language-icons/api-svgrepo-com.svg", alt: "api_icon", title: "API", trueFalse: "api_TF" },
     { iconImage: "/icons/other-icons/wouter-github-logo.svg", alt: "wouter_icon", title: "Wouter", trueFalse: "wouter_TF" },
     { iconImage: "/icons/language-icons/nextjs-fill-svgrepo-com.svg", alt: "nextjs_icon", title: "NextJS", trueFalse: "nextjs_TF" },
-    { iconImage: "/icons/language-icons/typescript-icon-svgrepo-com.svg", alt: "typescript_icon", title: "Typescript", trueFalse: "typescript_TF" },
+    { iconImage: "/icons/language-icons/typescript-icon-svgrepo-com.svg", alt: "typescript_icon", title: "TypeScript", trueFalse: "typescript_TF" },
     { iconImage: "/icons/language-icons/redux-svgrepo-com.svg", alt: "redux_icon", title: "Redux", trueFalse: "redux_TF" },
-    { iconImage: "/icons/language-icons/postgresql-svgrepo-com.svg", alt: "postgresql_icon", title: "PostgreSql", trueFalse: "postgresql_TF" },
+    { iconImage: "/icons/language-icons/postgresql-svgrepo-com.svg", alt: "postgresql_icon", title: "PostgreSQL", trueFalse: "postgresql_TF" },
     { iconImage: "/icons/language-icons/vue-svgrepo-com.svg", alt: "vue_icon", title: "Vue", trueFalse: "vue_TF" },
     { iconImage: "/icons/other-icons/figma-svgrepo-com.svg", alt: "figma_icon", title: "Figma", trueFalse: "figma_TF" },
+];
+
+export const techExp = [
+    { name: "HTML5", experience: "8+", projectCount: ""},
+    { name: "CSS3", experience: "7+", projectCount: ""},
+    { name: "JavaScript", experience: "6+", projectCount: ""},
+    { name: "SASS/SCSS", experience: "3+", projectCount: ""},
+    { name: "Astro", experience: "<1", projectCount: ""},
+    { name: "React", experience: "3+", projectCount: ""},
+    { name: "Tailwind CSS", experience: "2+", projectCount: ""},
+    { name: "Bootstrap", experience: "1+", projectCount: ""},
+    { name: "JSON", experience: "3+", projectCount: ""},
+    { name: "API", experience: "3+", projectCount: ""},
+    { name: "Wouter", experience: "2+", projectCount: ""},
+    { name: "NextJS", experience: "<1", projectCount: ""},
+    { name: "TypeScript", experience: "1+", projectCount: ""},
+    { name: "Redux", experience: "1+", projectCount: ""},
+    { name: "PostgreSQL", experience: "1+", projectCount: ""},
+    { name: "Vue", experience: "<1", projectCount: ""},
+    { name: "Figma", experience: "2+", projectCount: ""}
 ];
