fdb91542d35d09bbac0ca33722bb7c35721393f9
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-07-25 01:08:35 +0000

    Worked a bit more on tech stack section on home page.

--- src/components/home-page-components/HomePage_TechStack.jsx
+++ src/components/home-page-components/HomePage_TechStack.jsx
@@ -1,35 +1,61 @@
+'use client'
+import { useState } from 'react';
 import HomePageSectionLayout from "@/layouts/HomePageSectionLayout";
 import HomePageSectionHeader from "./HomePageSectionHeader";
-import { techIcons } from "@/utils/projectTechIcons";
+import { techIcons, techExp } from "@/utils/projectTechIcons";
 
 export default function HomePage_TechStack() {
+
+    const [selectedIcon, setSelectedIcon] = useState("/icons/general-icons/question-diamond-svgrepo-com.svg");
+    const [selectedName, setSelectedName] = useState("???");
+    const [selectedExp, setSelectedExp] = useState("");
+    const [selectedProj, setSelectedProj] = useState("");
+
+    const handleSelection = (index) => {
+        setSelectedIcon(techIcons[index].iconImage);
+        setSelectedName(techExp[index].name);
+        setSelectedExp(techExp[index].experience);
+        setSelectedProj(techExp[index].projectCount);
+    };
 
     return (
         <>
             <HomePageSectionLayout>
 
                 <HomePageSectionHeader>Technology</HomePageSectionHeader>
-                <div className="flex">
-                    <div className="w-3/5 flex justify-center flex-wrap gap-1 border-white border-2 border-solid">
+                <div className="flex flex-col-reverse custom-md:flex-row">
+
+                    <div className="w-full custom-md:w-3/5 flex justify-center flex-wrap gap-1 p-2">
                         <p className="mb-4 w-full text-center text-white font-semibold text-xl">
-                            Click on a technology to learn more about my experience
+                            Select a technology for more info.
                         </p>
                         {techIcons.map((tech, index) => (
-                            <div key={index} className="w-24 h-24 grid place-items-center bg-white bg-opacity-20 rounded-lg shadow-lg">
-                                <img src={tech.iconImage} alt="" className="w-16 h-16" title={tech.title} />
-                            </div>
+                            <button key={index} onClick={() => handleSelection(index)}
+                                className="w-16 custom-md:w-24 h-16 custom-md:h-24 grid place-items-center bg-white bg-opacity-20 rounded-lg shadow-lg hover:bg-opacity-50 transition-all duration-500 ease-in-out">
+                                <img src={tech.iconImage} alt="" className="w-12 custom-md:w-16 h-12 custom-md:h-16" title={tech.title} />
+                            </button>
                         ))}
                     </div>
 
-                    <div className="w-2/5 flex flex-col items-center border-red-500 border-2 border-solid">
-                        <img src="/icons/work-in-progress.png" className="w-36" />
 
-                        <img src="" alt="" className="" />
-                        <p></p>
-                        <p></p>
-                        <div></div>
-                        <div></div>
+                    <div className="w-full custom-md:w-2/5 grid place-content-center">
+                        <div className="mb-5 custom-md:mb-0 w-80 flex flex-col justify-center items-center font-theme-oxanium bg-white bg-opacity-25 p-6 rounded-2xl">
+
+                            <img src={selectedIcon} alt="" className="mb-4 w-36" />
+                            <p className="mb-4 font-bold text-2xl text-white">{selectedName}</p>
+
+                            <div className="mb-4 w-72 h-12 flex font-bold text-lg leading-[18px] border-white border-2 border-solid rounded-full overflow-hidden">
+                                <div className="w-1/2 h-full flex items-center text-black bg-white mix-blend-lighten pl-6">Experience</div>
+                                <div className="w-1/2 h-full flex justify-center items-center text-white">{selectedExp} year(s)</div>
+                            </div>
+
+                            <div className="w-72 h-12 flex font-bold text-lg leading-[18px] border-white border-2 border-solid rounded-full overflow-hidden">
+                                <div className="w-1/2 h-full flex items-center text-black bg-white mix-blend-lighten pl-6">Projects</div>
+                                <div className="w-1/2 h-full flex justify-center items-center text-white">{selectedProj}</div>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
 
             </HomePageSectionLayout>
