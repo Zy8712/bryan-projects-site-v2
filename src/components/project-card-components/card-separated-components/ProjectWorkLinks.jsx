1a5cbde69d1d8bb1af25ba61ff5c869aeda801b6
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-09-12 16:40:55 +0000

    Temp. patch missing links issue.
    Specifics:
    - applied temporary patch for detail links issue (when project is not marked as completed the details link leads to a page error)
    - projects with no links assigned to github repo, details, page preview will have the link hidden

--- src/components/project-card-components/card-separated-components/ProjectWorkLinks.jsx
+++ src/components/project-card-components/card-separated-components/ProjectWorkLinks.jsx
@@ -6,21 +6,21 @@ export default function ProjectWorkLinks(props) {
         <>
             <div className="w-full h-[50px] flex absolute bottom-0 px-5 sm:px-8">
 
-                <div className="w-1/4 h-full flex justify-start items-center">
+                <div className={`w-1/4 h-full flex justify-start items-center ${props.project_status != "Completed" ? 'opacity-0' : ''}`}>
                     <a className="link-custom" href={detailsLink} rel="noopener noreferrer">
                         <i className="las la-info-circle mr-1"></i>
                         Details
                     </a>
                 </div>
 
-                <div className="w-2/4 h-full flex justify-center items-center">
+                <div className={`w-2/4 h-full flex justify-center items-center ${props.links.github_link == "" ? 'opacity-0' : ''}`}>
                     <a className="link-custom" href={props.links.github_link} target="_blank" rel="noopener noreferrer">
                         <i className="lab la-github mr-1"></i>
                         GitHub Repo
                     </a>
                 </div>
 
-                <div className="w-1/4 h-full flex justify-end items-center">
+                <div className={`w-1/4 h-full flex justify-end items-center ${props.links.website_preview_link == "" ? 'opacity-0' : ''}`}>
                     <a className="link-custom" href={props.links.website_preview_link} target="_blank" rel="noopener noreferrer">
                         <i className="las la-external-link-alt mr-1"></i>
                         Preview
